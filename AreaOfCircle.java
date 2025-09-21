b248e42f2ddc1d6621d2916be3e29ac148fca722
Lakshya <varshneylakshya123@gmail.com>
2025-09-21 14:13:27 +0530

    Circle Area

---
+++ AreaOfCircle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class AreaOfCircle {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the radius:");
+        float rad = in.nextFloat();
+        System.out.print(rad);
+        float area = 28/7*(rad*rad);
+        System.out.println(area);
+       
+    }
+}
